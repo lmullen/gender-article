18e039cc898fc9e7eb653b5a0d7224fa9145294a
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-09-01 18:43:34 -0400

    Necessary packages

--- setup.R
+++ setup.R
@@ -1,6 +1,6 @@
 # Install the necessary packages
 
-install.packages(c("devtools", "knitr"), dependencies = TRUE)
+install.packages(c("devtools", "knitr", "packrat"), dependencies = TRUE)
 library(devtools)
 install_github("lmullen/gender-data-pkg")
 install_github("ropensci/gender", branch = "develop") # development version
